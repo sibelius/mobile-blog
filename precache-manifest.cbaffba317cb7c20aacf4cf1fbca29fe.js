4a8a1594f8a4082bbf8308d3ce65a473c9a0113d
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-06-22 08:29:37 -0300

    Updates

---
+++ precache-manifest.cbaffba317cb7c20aacf4cf1fbca29fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a54814a4504b6724f827f3d47f5b2c",
+    "url": "/mobile-blog/index.html"
+  },
+  {
+    "revision": "ae2b4c86441b71cd97f4",
+    "url": "/mobile-blog/static/js/2.2c2a7873.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mobile-blog/static/js/2.2c2a7873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d65b583284a2a13d71",
+    "url": "/mobile-blog/static/js/main.62198cb0.chunk.js"
+  },
+  {
+    "revision": "9efb6ac30f1e7e1a6a29",
+    "url": "/mobile-blog/static/js/runtime-main.a1092863.js"
+  },
+  {
+    "revision": "1a9c4a148f88aa97082b8caa4683b8c8",
+    "url": "/mobile-blog/static/media/nomad-3x.1a9c4a14.png"
+  }
+]);
